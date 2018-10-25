a1e6dfb3d06355d5761966fb8a6871985d706a00
Mick O Donnell <mickleod@gmail.com>
2018-10-25 20:48:29 +0100

    week4 lessons and exercises

---
+++ week4/string_case_counter_puzzle.py
@@ -0,0 +1,16 @@
+#Write a Python function that accepts a string 
+#and calculate the number of upper case letters and lower case letters. 
+
+    print ("Original String : ", s)
+        elif c.islower():
+string_test('The quick Brow Fox')
+    print ("No. of Upper case characters : ", d["UPPER_CASE"])
+        if c.isupper():
+           d["UPPER_CASE"]+=1
+    d={"UPPER_CASE":0, "LOWER_CASE":0}
+    print ("No. of Lower case Characters : ", d["LOWER_CASE"])
+def string_test(s):
+           d["LOWER_CASE"]+=1
+    for c in s:
+        else:
+           pass
