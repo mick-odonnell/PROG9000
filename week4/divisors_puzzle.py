b32193f386b9f533cd2e82631fe9a4d479c5580d
Mick O Donnell <mickleod@gmail.com>
2018-11-08 16:34:15 +0000

    Update divisors_puzzle.py

--- week4/divisors_puzzle.py
+++ week4/divisors_puzzle.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # Write a Python program to find those numbers which are divisible by 7 and 
 # multiple of 5, between 1500 and 2700 (both included)."
 
@@ -6,13 +6,4 @@ for x in range(1500, 2701):
 nl=[]
 print (','.join(nl))
     if (x%7==0) and (x%5==0):
-=======
-# Write a Python program to find those numbers which are divisible by 7 and 
-# multiple of 5, between 1500 and 2700 (both included)."
-
-for x in range(1500, 2701):
-nl=[]
-print (','.join(nl))
-    if (x%7==0) and (x%5==0):
->>>>>>> a9e0f257ba400aface440d84b65048f41a9707e1
         nl.append(str(x))
