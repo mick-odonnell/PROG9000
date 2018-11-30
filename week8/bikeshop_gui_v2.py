513f7e26e0558f33912e3e55dd4c29fa91e2fbaf
Mick O Donnell <mickleod@gmail.com>
2018-11-30 14:07:52 +0000

    wk8

---
+++ week8/bikeshop_gui_v2.py
@@ -0,0 +1,42 @@
+from PyQt5 import uic, QtWidgets
+import sys
+
+
+class ShopWidget(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ShopWidget, self).__init__()
+        uic.loadUi('bike_shop_v2.ui', self)
+        
+        self.btn_submit.clicked.connect(self.btn_push)
+        
+        self.le_username.textChanged.connect(self.le_username_changed)
+
+
+    #----------------------------------
+    # gui variables
+    #----------------------------------
+
+        self.basket = {}
+
+    #----------------------------------
+    # widget actions
+    #----------------------------------
+    def btn_push(self):
+        if self.sb_quantity.value():
+            self.basket[self.cb_item.currentText()] = self.sb_quantity.value()
+            print('1')
+        basket_str_op = ''
+        for k, v in self.basket.items():
+            basket_str_op += k + ":    " + str(v) + "\n"
+        self.tb_basket.setPlainText(basket_str_op)
+        
+    def le_username_changed(self):
+        self.btn_submit.setEnabled(True)
+        
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = ShopWidget()
+    window.show()
+    sys.exit(app.exec_())
