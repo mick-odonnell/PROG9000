e78d79c9c92e23434bb8aa74367897f2fa6ca807
Mick O Donnell <mickleod@gmail.com>
2018-12-07 13:42:40 +0000

    Update bank_ac.py

--- week9/bank_ac.py
+++ week9/bank_ac.py
@@ -1,3 +1,12 @@
+# This is a program to manage money for a school back
+# We have built some classes and some methods which we will want
+# to use again
+
+import os
+import csv
+
+# Create a bank account class that contains the following attributes and methods: Attributes: name , address,
+# account_number, balance, pending_pos, pending_neg Methods: funds_available (balance+pending_pos – pending_neg)
 
 class BankAccount:
     def __init__(self, name , address, account_number, balance, overdraft = 0):
@@ -13,3 +22,9 @@ class BankAccount:
             self.ac_balance -= amount
         print("-------------------------")
         print("Your new balance is ", self.ac_balance)
+
+def main():
+    # this is where i will test stuff
+
+if __name__ == '__main__':
+    main()
